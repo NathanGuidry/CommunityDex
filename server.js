45bdb4643938ec6b08bdc3b754598d95ee46f22d
Nathan Guidry <nathanguidry10@gmail.com>
2022-12-25 01:31:53 -0600

    error handling and template

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const mongoose = require('mongoose')
 import { Pokemon } from './models/Pokemon.js'
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
+import { ExpressError } from './utils/ExpressError.js'
+import { catchAsync } from './utils/catchAsync.js'
 const numOfSpecialPokemon = 249
 const numOfBasePokemon = 905
 
@@ -39,9 +41,11 @@ const nameChecker = async function (req, res, next) {
     const basePokemon = await P.getPokemonsList()
     const baseNames = basePokemon.results.slice(0, -numOfSpecialPokemon)
     const { name } = req.body
+    const err = {}
+    err.message = 'A pokemon with this name already exists'
     for (let i = 0; i < numOfBasePokemon; i++) {
         if (name.toLowerCase() === baseNames[i].name) {
-            res.send("A pokemon with that name already exists")
+            res.render('error', { err })
             return
         }
     }
@@ -62,130 +66,99 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/userPokemon', async (req, res) => {
-    try {
-        const userPokemon = await Pokemon.find({})
-        res.render('pokemon/userPokemon', { userPokemon })
-    }
-    catch (e) {
-        console.log(e)
-        res.send('error')
-    }
-})
+app.get('/userPokemon', catchAsync(async (req, res) => {
+    const userPokemon = await Pokemon.find({})
+    res.render('pokemon/userPokemon', { userPokemon })
+}))
 
-app.get('/pokemon', async (req, res) => {
-    try {
-        const pokemon = await P.getPokemonsList()
-        const pokemonNames = pokemon.results.slice(0, -numOfSpecialPokemon)
-        const userPokemon = await Pokemon.find({})
-        res.render('pokemon/index', { pokemonNames, userPokemon })
-    }
-    catch (e) {
-        console.log(e)
-        res.send('error')
-    }
-})
+app.get('/pokemon', catchAsync(async (req, res) => {
+    const pokemon = await P.getPokemonsList()
+    const pokemonNames = pokemon.results.slice(0, -numOfSpecialPokemon)
+    const userPokemon = await Pokemon.find({})
+    res.render('pokemon/index', { pokemonNames, userPokemon })
+}))
 
-app.post('/pokemon', nameChecker, async (req, res) => {
-    try {
-        const { pokedexNum, name, type1, type2, description } = req.body
-        if (type2) {
-            const newPokemon = new Pokemon({ pokedexNum, name, type1, type2, description })
-            await newPokemon.save()
-            res.redirect('/userPokemon')
-        }
-        else {
-            const newPokemon = new Pokemon({ pokedexNum, name, type1, description })
-            await newPokemon.save()
-            res.redirect('/userPokemon')
-        }
-    }
-    catch (e) {
-        console.log(e)
-        res.send('error')
-    }
-})
-
-app.get('/pokemon/new', async (req, res) => {
-    try {
-        const maxId = await Pokemon.findOne({}).sort({ pokedexNum: -1 })
-        res.render('pokemon/new', { maxId })
-    }
-    catch (e) {
-        console.log(e)
-        res.send('error')
-    }
-})
-
-app.get('/pokemon/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        if (id <= numOfBasePokemon) {
-            const pokemon = await P.getPokemonByName(id)
-            const description = await englishDesc(id)
-            res.render('pokemon/show', { pokemon, id, description, numOfBasePokemon })
-        }
-        else {
-            let pokemon = await Pokemon.find({ pokedexNum: id })
-            pokemon = pokemon[0]
-            const description = pokemon.description
-            res.render('pokemon/show', { pokemon, id, description, numOfBasePokemon })
-        }
-    }
-    catch (e) {
-        console.log(e)
-        res.send('error')
-    }
-})
-
-app.delete('/pokemon/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const pokemon = await Pokemon.findOneAndDelete({ pokedexNum: id })
-        const remainingPokemon = await Pokemon.find({ pokedexNum: { $gt: id } })
-        for (let pokemon of remainingPokemon) {
-            pokemon.pokedexNum = pokemon.pokedexNum - 1
-            await pokemon.save()
-        }
+app.post('/pokemon', nameChecker, catchAsync(async (req, res) => {
+    const { pokedexNum, name, type1, type2, height, weight, description } = req.body
+    if (type2) {
+        const newPokemon = new Pokemon({ pokedexNum, name, type1, type2, height, weight, description })
+        await newPokemon.save()
         res.redirect('/userPokemon')
     }
-    catch (e) {
-        console.log(e)
-        res.send('error')
+    else {
+        const newPokemon = new Pokemon({ pokedexNum, name, type1, height, weight, description })
+        await newPokemon.save()
+        res.redirect('/userPokemon')
     }
-})
+}))
 
-app.get('/pokemon/:id/edit', async (req, res) => {
-    try {
-        const { id } = req.params
+app.get('/pokemon/new', catchAsync(async (req, res) => {
+    const maxId = await Pokemon.findOne({}).sort({ pokedexNum: -1 })
+    res.render('pokemon/new', { maxId })
+}))
+
+app.get('/pokemon/:id', catchAsync(async (req, res) => {
+    const { id } = req.params
+    if (id <= numOfBasePokemon) {
+        const pokemon = await P.getPokemonByName(id)
+        const description = await englishDesc(id)
+        res.render('pokemon/show', { pokemon, id, description, numOfBasePokemon })
+    }
+    else {
         let pokemon = await Pokemon.find({ pokedexNum: id })
         pokemon = pokemon[0]
-        res.render('pokemon/edit', { id, pokemon })
+        const description = pokemon.description
+        res.render('pokemon/show', { pokemon, id, description, numOfBasePokemon })
     }
-    catch (e) {
-        console.log(e)
-        res.send('error')
+}))
+
+app.delete('/pokemon/:id', catchAsync(async (req, res) => {
+    const { id } = req.params
+    const pokemon = await Pokemon.findOneAndDelete({ pokedexNum: id })
+    const remainingPokemon = await Pokemon.find({ pokedexNum: { $gt: id } })
+    for (let pokemon of remainingPokemon) {
+        pokemon.pokedexNum = pokemon.pokedexNum - 1
+        await pokemon.save()
     }
+    res.redirect('/userPokemon')
+}))
+
+app.get('/pokemon/:id/edit', catchAsync(async (req, res) => {
+    const { id } = req.params
+    let pokemon = await Pokemon.find({ pokedexNum: id })
+    pokemon = pokemon[0]
+    res.render('pokemon/edit', { id, pokemon })
+}))
+
+app.patch('/pokemon/:id', nameChecker, catchAsync(async (req, res) => {
+    const { id } = req.params
+    let { name, type1, type2, description } = req.body
+    if (type2 === '') {
+        const pokemon = await Pokemon.findOneAndUpdate({ pokedexNum: id }, { name, type1, $unset: { type2: "" }, description }, { runValidators: true, new: true })
+    }
+    else {
+        const pokemon = await Pokemon.findOneAndUpdate({ pokedexNum: id }, { name, type1, type2, description }, { runValidators: true, new: true })
+    }
+    res.redirect(`/pokemon/${id}`)
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 })
 
-app.patch('/pokemon/:id', nameChecker, async (req, res) => {
-    try {
-        const { id } = req.params
-        let { name, type1, type2, description } = req.body
-        if (type2 === '') {
-            const pokemon = await Pokemon.findOneAndUpdate({ pokedexNum: id }, { name, type1, $unset: { type2: "" }, description }, { runValidators: true, new: true })
-        }
-        else {
-            const pokemon = await Pokemon.findOneAndUpdate({ pokedexNum: id }, { name, type1, type2, description }, { runValidators: true, new: true })
-        }
-        res.redirect(`/pokemon/${id}`)
-    }
-    catch (e) {
-        console.log(e)
-        res.send('error')
-    }
+const handleDuplicateKey = err => {
+    return new ExpressError('A pokemon with that name already exists', 500)
+}
 
+app.use((err, req, res, next) => {
+    console.dir(err)
+    if (err.name === 'MongoServerError') err = handleDuplicateKey(err)
+    next(err)
+})
 
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'Something went wrong' } = err
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
